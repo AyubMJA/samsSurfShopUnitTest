b0f27376e6462ba71d6b41efb3aa1c6ed6a7cddd
Ayub Ali <ayubonline8448@icloud.com>
2022-07-25 13:00:18 -0400

    I wrote two methods that would fail

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-from surfshop import surfshop
+import surfshop
 import unittest
 
+class SurfShopTest(unittest.TestCase):
+    def setUp(self):
+        self.cart = surfshop.ShoppingCart()
+
+
+    def test_add_surfboard(self):
+        message = self.cart.add_surfboards(quantity=1)
+        self.assertEqual(message, f'Successfully added 1 surf board to cart!')
+
+    def test_add_surfboards(self):
+        for i in range(2,5):
+            with self.subTest(i=i):
+                message = self.cart.add_surfboards(i)
+                self.assertEqual(message, f'Successfully add {i} surfboards to cart!')
+                self.cart = surfshop.ShoppingCart()
+
+unittest.main()
